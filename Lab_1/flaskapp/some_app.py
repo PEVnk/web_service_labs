d6c21f7d1c5ac5c231c70d511b42b53c8fcdc501
PEVnk <perkova.ev@gmail.com>
2025-10-19 22:56:51 +0700

    Update some_app.py

--- Lab_1/flaskapp/some_app.py
+++ Lab_1/flaskapp/some_app.py
@@ -9,11 +9,13 @@ def hello():
 #наша новая функция сайта
 @app.route("/data_to")
 def data_to(): 
-  #создаем переменные с данными для передачи в шаблон some_pars = {'user':'Ivan','color':'red'}
-  some_str = 'Hello my dear friends!' 
-  some_value = 10
-  #передаем данные в шаблон и вызываем его 
-  return render_template('simple.html',some_str = some_str,
-  some_value = some_value,some_pars=some_pars)
+    #создаем переменные с данными для передачи в шаблон some_pars = {'user':'Ivan','color':'red'}
+    some_str = 'Hello my dear friends!' 
+    some_value = 10
+    #передаем данные в шаблон и вызываем его 
+    return render_template('simple.html',
+                           some_str = some_str,
+                           some_value = some_value,
+                           some_pars=some_pars)
 if __name__ == "__main__":
-  app.run(host='127.0.0.1',port=5000)
+    app.run(host='127.0.0.1',port=5000)
