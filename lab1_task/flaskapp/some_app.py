31f68d08d5efd2d2cdd4a8bfaf309c97de047b2c
PEVnk <perkova.ev@gmail.com>
2025-10-26 19:25:22 +0700

    Update some_app.py

--- lab1_task/flaskapp/some_app.py
+++ lab1_task/flaskapp/some_app.py
@@ -14,7 +14,7 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 app.secret_key = 'your-secret-key-here'
 
 # Конфигурация Google reCAPTCHA
-RECAPTCHA_SECRET_KEY = ''6LcNBvIrAAAAAL691ETA9MMl-3FUSVcZ1qFEHLtR'  
+RECAPTCHA_SECRET_KEY = '6LcNBvIrAAAAAL691ETA9MMl-3FUSVcZ1qFEHLtR'  
 RECAPTCHA_SITE_KEY = '6LcNBvIrAAAAAJJeUvIG4zgJ8v5RMgiqP5yMyaMl'      
 
 def verify_recaptcha(recaptcha_response):
